e69b83825a3a7132b76c5b1cb16553c278fd8e64
R. M. Mauney <rmmauney@gmail.com>
2018-07-01 19:20:55 -0400

    lesson 7 Post Meta Data

--- index.php
+++ index.php
@@ -7,6 +7,30 @@ if (have_posts()) :
 
     <article class="post">
         <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+
+        <p class="post-info">
+            <?php the_time('F jS, Y'); ?> | by 
+            <?php the_author_posts_link(); ?> | posted in
+
+            <?php
+
+            $categories = get_the_category();
+            $separator = ", ";
+            $output = "";
+
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+                    
+                }
+            }
+
+            echo trim($output, $separator);
+
+            ?>
+
+        </p>
+        
         <?php the_content() ?>
     </article>
 
