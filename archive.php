a2cfd5fc432d3f88d01b1f583e792e78480150d4
R. M. Mauney <rmmauney@gmail.com>
2018-07-03 16:04:55 -0400

    lesson 12 get_template_part tutorial

--- archive.php
+++ archive.php
@@ -29,41 +29,14 @@ if (have_posts()) :
     ?></h2>
 
     <?php
-    while (have_posts()) : the_post(); ?>
+    while (have_posts()) : the_post();
 
-    <article class="post">
-        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    get_template_part('content');
 
-        <p class="post-info">
-            <?php the_time('F jS, Y'); ?> | by 
-            <?php the_author_posts_link(); ?> | posted in
+    endwhile;
 
-            <?php
-
-            $categories = get_the_category();
-            $separator = ", ";
-            $output = "";
-
-            if ($categories) {
-                foreach ($categories as $category) {
-                    $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-                    
-                }
-            }
-
-            echo trim($output, $separator);
-
-            ?>
-
-        </p>
-        
-        <?php the_excerpt(); ?>
-    </article>
-
-<?php endwhile;
-
-else :
-    echo '<p>No content found.</p>';
+    else :
+        echo '<p>No content found.</p>';
 
 endif;
 
--- index.php
+++ index.php
@@ -3,50 +3,14 @@
 get_header();
 
 if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
+    while (have_posts()) : the_post();
 
-    <article class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail<?php } ?>">
+    get_template_part('content');
 
-        <div class="post-thumbnail">
-            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
-        </div>
+    endwhile;
 
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-        <p class="post-info">
-            <?php the_time('F jS, Y'); ?> | by 
-            <?php the_author_posts_link(); ?> | posted in
-
-            <?php
-
-            $categories = get_the_category();
-            $separator = ", ";
-            $output = "";
-
-            if ($categories) {
-                foreach ($categories as $category) {
-                    $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-                    
-                }
-            }
-
-            echo trim($output, $separator);
-
-            ?>
-
-        </p>
-        
-        <p>
-            <?php echo get_the_excerpt(); ?>
-            <a href="<?php the_permalink() ?>">Read more&raquo;</a>
-        </p>
-
-    </article>
-
-<?php endwhile;
-
-else :
-    echo '<p>No content found.</p>';
+    else :
+        echo '<p>No content found.</p>';
 
 endif;
 
